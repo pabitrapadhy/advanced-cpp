903eefa878010107ee518b79c77395de8bf5b2a5
Pabitra Padhy <pabitrapadhycse@gmail.com>
2018-09-18 11:39:15 +0530

    Initial Commit

---
+++ advanced_cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  advanced_cpp
+//
+//  Created by Padhy, Pabitra on 18/09/18.
+//  Copyright © 2018 Padhy, Pabitra. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
